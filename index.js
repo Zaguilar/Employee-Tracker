ce7ec559bd698626f0f43c271de5838465b9a80e
Zachary Aguilar <zacharyjag@yahoo.com>
2020-07-11 18:18:17 -0500

    js update

--- index.js
+++ index.js
@@ -13,69 +13,28 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
+  microManage();
 });
 
-function promptUser(){
-    return inquirer.prompt(
+
+async function microManage(){
+    console.log("It's time to micro-manage!")
+   await inquirer.prompt(
         [
             {
-                type: "input",
-                message: "What is your README title?",
-                name: "title"
-            },
-            {
-                type: "input",
-                message: "Enter a brief description.",
-                name: "description"
-            },
-            {
-                type: "checkbox",
-                message: "Enter your table of contents.",
-                name: "tableContents",
-                choices: ["[Description](#description)",
-                 "[Installation](#installation)", 
-                 "[Usage](#usage)", 
-                 "[License](#license)", 
-                 "[Contributing](#contributing)",
-                 "[Tests](#tests)",
-                 "[Questions](#questions)"]
-            },
-            {
-              type: "input",
-              message: "Enter your installation instructions",
-              name: "install" 
-            },
-            {
-              type: "input",
-              message: "Enter usage information",
-              name: "usage"  
-            },
-            {
+                name: "choice",
                 type: "list",
-                message: "License?",
-                name: "licensing",
-                choices: ["[MIT](https://choosealicense.com/licenses/mit/)", "ISC"]
-            },
-            {
-                type: "input",
-                message: "Contributing guidelines",
-                name: "contribute" 
-            },
-            {
-                type: "input",
-                message: "Tests",
-                name: "test"
-            },
-            {
-                type: "input",
-                message: "What's your Github username?",
-                name: "gitHubUrl"
-            },
-            {
-                type: "input",
-                message: "What is your Email?",
-                name: "email"
+                message: "Where do you want to go?",
+                choices: [
+                    "Add department",
+                    "View department",
+                    "Add role",
+                    "View roles",
+                    "Add employee",
+                    "View employees",
+                    "Update roles",
+                    "Exit"
+                ]
             }
-        ]
-    );
-}
+        
+        ])};
